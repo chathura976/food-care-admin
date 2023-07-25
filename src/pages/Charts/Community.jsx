b0b9a776e041d403b4ceb38bc68b3db17d2d0218
Supun980307 <madusankapms1@gmail.com>
2023-07-25 23:35:22 +0530

    Community

---
+++ src/pages/Charts/Community.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout } from '../components';
+const Community = () => {
+  return (
+    <>
+    <Layout>
+      <div>Forum</div>
+    </Layout>
+    
+    </>
+  )
+}
+
+
+
+export default Community
